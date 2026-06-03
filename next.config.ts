29ab4789a60ababfffd4efc2ad76c32aa88c93de
Shamel Rai <shamel.rai@gmail.com>
2026-06-03 17:46:06 +0545

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  deploymentId: process.env.DEPLOYMENT_VERSION,
+  output: "standalone",
+};
+
+export default nextConfig;
